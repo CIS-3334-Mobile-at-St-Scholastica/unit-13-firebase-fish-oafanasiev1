157b2bd112c80a3b39cdc9cf76c28f23ede52145
Alex Afanasiev <oafanasiev@css.edu>
2019-04-24 19:31:14 -0500

    Participation 13 Completed

--- app/src/main/java/edu/css/unit13_firebasefish_2018/MainActivity.java
+++ app/src/main/java/edu/css/unit13_firebasefish_2018/MainActivity.java
@@ -10,6 +10,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,15 +26,22 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<Fish> fishAdapter;
     List<Fish> fishList;
     FishFirebaseData fishDataSource;
-//    DatabaseReference myFishDbRef;
+    DatabaseReference myFishDbRef;
     int positionSelected;
     Fish fishSelected;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
+        myRef.setValue("Hello, World!");
+
+      //  mAuth = FirebaseAuthException.
         //setupFirebaseDataChange();
         setupListView();
         setupAddButton();
@@ -35,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         setupDeleteButton();
     }
 
-    /*
+
     private void setupFirebaseDataChange() {
         fishDataSource = new FishFirebaseData();
         myFishDbRef = fishDataSource.open();
@@ -55,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    */
+
 
     private void setupListView() {
         listViewFish = (ListView) findViewById(R.id.ListViewFish);
